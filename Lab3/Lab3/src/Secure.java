49c94c10777bf45661a70108533d5b43ae5e8f42
Loual201 <louise.almer97@gmail.com>
2020-09-25 12:10:17 +0200

    Lägger till för lab3

---
+++ Lab3/Lab3/src/Secure.java
@@ -0,0 +1,12 @@
+
+public class Secure {
+
+}
+
+//Connect to a server using secure connection (SSL/TSL)
+//the client must authenticate the server
+//the server must authenticate the client
+//File exchange is encrypted 
+//client and server communicate via strong cryptographic algorithms
+//client and server have distinct keystores and truststores
+//Client should be able to download/delete and upload files from the server
